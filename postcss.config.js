f544cd2a531219c7ca65e5e44f6837b3d7e7976d
Maria Senina <maria.senina@confirmit.com>
2019-10-15 15:00:09 +0300

    variables.css

--- postcss.config.js
+++ postcss.config.js
@@ -1,2 +1,12 @@
 module.exports = {
-}
+    plugins: {
+        'postcss-import': {},
+        'postcss-custom-properties': {
+            preserve: false
+        },
+        'postcss-preset-env': {
+            browsers: 'last 2 versions',
+            stage: 0,
+        }
+    }
+};
